be0bfcc65d630d0a4d6ca0d7ddfd5e8260362f8d
David Knipe <david.knipe@episerver.com>
2015-09-14 17:41:56 +0100

    v1.0.2 .net version change
    Drop the minimum .net version from 4.5.1 to 4.5 for maximum
    compatibility

--- Nuget/pack.bat
+++ Nuget/pack.bat
@@ -1,5 +1,5 @@
 @Echo Setting up folder structure
-md Package\lib\net451\
+md Package\lib\net45\
 md Package\tools\
 md Package\content\
 
@@ -9,13 +9,13 @@ md Package\content\lang\
 
 
 @Echo Removing old files
-del /Q Package\lib\net451\*.*
+del /Q Package\lib\net45\*.*
 del /Q Package\tools\*.*
 del /Q Package\content\*.*
 
 @Echo Copying new files
 copy ..\VisitorGroupUsage\module.config.transform Package\content
-copy ..\VisitorGroupUsage\bin\Release\VisitorGroupUsage.dll Package\lib\net451
+copy ..\VisitorGroupUsage\bin\Release\VisitorGroupUsage.dll Package\lib\net45
 copy ..\VisitorGroupUsage\ClientResources\Scripts\visitorgroupusage\viewer.js Package\content\ClientResources\Scripts\visitorgroupusage\viewer.js
 copy ..\VisitorGroupUsage\Views\VisitorGroupUsage\Index.cshtml Package\content\Views\VisitorGroupUsage\Index.cshtml
 copy ..\VisitorGroupUsage\Views\VisitorGroupUsage\VisitorGroupListing.cshtml Package\content\Views\VisitorGroupUsage\VisitorGroupListing.cshtml
